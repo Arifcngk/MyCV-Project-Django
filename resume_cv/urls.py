ebb2330164b2285fc9b6cf9e64b0621529f73c0b
Arifcngk <93188117+Arifcngk@users.noreply.github.com>
2024-09-10 02:14:48 +0300

    Add initial project files and structure

---
+++ resume_cv/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('home', include("core.urls")),
+    path('', include("core.urls")),
+
+
+]
